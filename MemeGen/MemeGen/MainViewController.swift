b0692c4335796f5fbe1873c024ee48b282fd8558
RunningRaccoon <kate@last30min.com>
2021-10-21 12:34:31 +0900

    proj done

--- MemeGen/MemeGen/MainViewController.swift
+++ MemeGen/MemeGen/MainViewController.swift
@@ -22,14 +22,36 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
-        configureLayout()
+        bind()
     }
     
-    private func configureLayout() {
+    private func bind() {
         
+        viewModel.displayImage
+            .asObservable()
+            .observe(on: MainScheduler.instance)
+            .bind(to: displayImageView.rx.image)
+            .disposed(by: disposeBag)
+        
+        clearButton.rx.tap
+            .bind(to: viewModel.onResetTap)
+            .disposed(by: disposeBag)
+        
+        tableView.rx.itemSelected
+            .map { $0.row }
+            .bind(to: viewModel.onThumbnailTap)
+            .disposed(by: disposeBag)
     }
     
     private func configureTableView() {
+        Observable.of(viewModel.thumbnails)
+            .bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: UITableViewCell.self)) {
+                row, element, cell in
+                cell.imageView?.image = element
+            }
+            .disposed(by: disposeBag)
         
+        tableView.rowHeight = 100
+        tableView.separatorStyle = .none
     }
 }
--- MemeGen/MemeGen/MainViewModel.swift
+++ MemeGen/MemeGen/MainViewModel.swift
@@ -6,10 +6,54 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class MainViewModel {
     
-    init() {
+    // Bind
+    let displayImage: Observable<UIImage>
+    let onResetTap = PublishSubject<Void>()
+    let onThumbnailTap = PublishSubject<Int>()
+    let thumbnails = [#imageLiteral(resourceName: "sandra-annerberg"), #imageLiteral(resourceName: "meme0"), #imageLiteral(resourceName: "meme1")]
+    
+    // Private
+    private let selectedImage = BehaviorRelay<UIImage?>(value: nil)
+    private let disposeBag = DisposeBag()
+    private let scheduler: SchedulerType
+    
+    init(scheduler: SchedulerType = MainScheduler.instance) {
+        self.scheduler = scheduler
         
+        displayImage = selectedImage
+            .map { $0 ?? Placeholder() }
+            .asObservable()
+        
+        bind()
+    }
+    
+    private func bind() {
+        onResetTap
+            .map { nil }
+            .bind(to: selectedImage)
+            .disposed(by: disposeBag)
+        
+        onThumbnailTap
+            .map { [thumbnails] in // 외부 변수 캡처리스트 사용
+                thumbnails[safe: $0]
+            }
+            .flatMap {
+                $0.map(Observable.just) ?? .empty()
+//                Observable.just($0)
+            }
+            .bind(to: selectedImage)
+            .disposed(by: disposeBag)
     }
 }
+
+extension Array {
+    subscript(safe index: Int) -> Element? {
+        return indices ~= index ? self[index] : nil
+    }
+}
+
